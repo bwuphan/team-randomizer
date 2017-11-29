b16af941721cc3aaa82f34ad2818f8a868c1e870
bobby <bwuphan@gmail.com>
2017-11-28 22:14:22 -0800

    Add button and text to homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { ButtonToolbar, Button } from 'react-bootstrap'
 import './App.css';
-import { Button } from 'react-bootstrap';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Team Randomizer</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="app-body">
+          <p className="App-intro">
+            Click here to get started
+          </p>
+          <Button bsStyle="success" bsSize="large">Large button</Button>
+        </div>
       </div>
     );
   }
