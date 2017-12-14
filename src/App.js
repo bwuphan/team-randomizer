b50c2949f7283957a95d3a653bfcbcabdb66f00f
bwuphan <bwuphan@gmail.com>
2017-12-13 18:17:55 -0700

    Use node_modules bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './Header';
 import Start from './Start';
